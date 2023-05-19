929c0b518cecff4cc6c30d2b02a60e86e890c7c3
maiton <donmaiton@gmail.com>
2023-05-19 23:14:27 +0900

    edit db conf

--- study/study/settings.py
+++ study/study/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'study.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysqldb',
+        'USER': 'hoge',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
